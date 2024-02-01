702a6e3a1a1208011aac20f937f96a0223ecc5ad
Asal <88582755+afarajlou@users.noreply.github.com>
2024-02-01 13:23:31 +0330

    Add files via upload

---
+++ Lab7/Group1/FullAdder_1_bit.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:14:48 11/26/2023 
+// Design Name: 
+// Module Name:    FullAdder_1_bit 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module FullAdder_1_bit(a, b, sel, Cin, Cout, Sum);
+	input a, b, sel, Cin;
+	output Sum, Cout;
+	wire Xor1, Xor2, And1, And2;
+	
+	xor
+		#(10) X1 (Xor1, b, sel),
+		X2 (Xor2, a, Xor1),
+		X3 (Sum, Xor2, Cin);
+	
+	and
+		#(5) A1 (And1, Xor2, Cin),
+		A2 (And2, a, Xor1);
+	
+	or #(5) O1(Cout, And1, And2);
+endmodule
+
+
---
+++ Lab7/Group1/FullAdder_4_bit.v
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:17:34 11/26/2023 
+// Design Name: 
+// Module Name:    FullAdder_4_bit 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module FullAdder_4_bit(A, B, sel, Cout, Sum);
+	input [3:0] A, B;
+	input sel;
+	output [3:0] Sum;
+	output Cout;
+	wire [2:0] e;
+	
+	FullAdder_1_bit 
+					F1 (A[0], B[0], sel, sel, e[0], Sum[0]),
+					F2 (A[1], B[1], sel, e[0], e[1], Sum[1]),
+					F3 (A[2], B[2], sel, e[1], e[2], Sum[2]),
+					F4 (A[3], B[3], sel, e[2], Cout, Sum[3]);
+endmodule
+
+
